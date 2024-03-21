c0914af866ce650f48a280a940fe680e77bdc5cb
Chuong Pham <phchuong1998@gmail.com>
2024-03-21 16:05:10 -0400

    added stuff

--- Lab5/Course_Dean.java
+++ Lab5/Course_Dean.java
@@ -2,17 +2,17 @@ import java.util.ArrayList;
 
 public class Course_Dean {
 
-	private ArrayList<Student> roster; // Collection of Student objects
+	private ArrayList<Student_Dean> roster; // Collection of Student objects
 
 	public Course_Dean() {
-		roster = new ArrayList<Student>();
+		roster = new ArrayList<Student_Dean>();
 	}
    
    // Return an ArrayList of all Students with a GPA of at least 3.5
    // This is like a Getter for an ArrayList
-	public ArrayList<Student> getDeansList() {
+	public ArrayList<Student_Dean> getDeansList() {
 	   /* Type your code here */
-	   ArrayList<Student> DeansList = new ArrayList<Student>();
+	   ArrayList<Student_Dean> DeansList = new ArrayList<Student_Dean>();
       for (int i = 0; i < roster.size(); ++i) 
       {
          if (roster.get(i).getGPA() >= 3.5)
@@ -23,14 +23,14 @@ public class Course_Dean {
       return DeansList;
 	}
 
-	public void addStudent(Student s) {
+	public void addStudent(Student_Dean s) {
 		roster.add(s);
 	}
 	
 	// main
    public static void main(String args[]) {
       Course_Dean course = new Course_Dean();
-      ArrayList<Student> deanList = new ArrayList<Student>();
+      ArrayList<Student_Dean> deanList = new ArrayList<Student_Dean>();
       String first;  // first name
       String last;   // last name
       double gpa;    // grade point average
@@ -38,26 +38,26 @@ public class Course_Dean {
       first = "Henry";
       last = "Nguyen";
       gpa = 3.5;
-      course.addStudent(new Student(first, last, gpa));  // Add 1st student
+      course.addStudent(new Student_Dean(first, last, gpa));  // Add 1st student
       
       first = "Brenda";
       last = "Stern";
       gpa = 2.0;
-      course.addStudent(new Student(first, last, gpa));  // Add 2nd student
+      course.addStudent(new Student_Dean(first, last, gpa));  // Add 2nd student
       
       first = "Lynda";
       last = "Robison";
       gpa = 3.2;
-      course.addStudent(new Student(first, last, gpa));  // Add 3rd student
+      course.addStudent(new Student_Dean(first, last, gpa));  // Add 3rd student
       
       first = "Sonya";
       last = "King";
       gpa = 3.9;
-      course.addStudent(new Student(first, last, gpa));  // Add 4th student
+      course.addStudent(new Student_Dean(first, last, gpa));  // Add 4th student
 
       deanList = course.getDeansList();
 		System.out.println("Dean's list:");
-		for (Student student: deanList) {
+		for (Student_Dean student: deanList) {
 			System.out.println(student);     // Expect: ArrayList with Henry and Sonya
 		}
    }    
---
+++ Tutorial/Generics/Generics.java
@@ -0,0 +1,47 @@
+public class Generics{
+    // public static void printStringArray (String[] myArray)
+    // {
+    //     for (String e:myArray)
+    //     {
+    //         System.out.println(e);
+    //     }
+    // }
+
+    // public static void printIntegerArray (int[] myArray)
+    // {
+    //     for (int e:myArray)
+    //     {
+    //         System.out.println(e);
+    //     }
+    // }    
+
+    public static <T> void printArray (T[] myArray) // Can name whatever inside <>, conventionally use 'T'
+    {
+        for (T e:myArray)
+        {
+            System.out.println(e);
+        }
+    } 
+
+    public static void main (String[] args){
+        String[] s = new String[3];
+        s[0] = "Test3";
+        s[1] = "Test1";
+        s[2] = "Test2";
+        // printStringArray(s);
+        printArray(s);
+
+        Integer[] i = new Integer[4]; // Generic cannot use primitive type
+        i[0] = 1;
+        i[1] = 3;
+        i[2] = 4;
+        i[3] = 5;    
+        // printIntegerArray(i);
+        printArray(i);
+
+        Person[] persons = new Person[2];
+        person[0] = new Person("Chuong", 21);
+        person[1] = new Person("Bob", 99);
+        printArray(persons);
+    }
+}
