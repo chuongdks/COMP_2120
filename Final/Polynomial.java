86a44a5cc3fd2d99aee6b840a1f2a11eadc36509
Chuong Pham <phchuong1998@gmail.com>
2024-04-03 01:33:52 -0400

    Added add() method

--- Final/Polynomial.java
+++ Final/Polynomial.java
@@ -25,7 +25,7 @@ public class Polynomial implements Comparable<Polynomial> {
     * The type of the Map for the instance filed terms must be TreeMap, which can keep the order
     * of the polynomial terms.
     */
-    // Default Constructor, create empty Polynomial
+    // Default Constructor, create empty Polynomial Class
     public Polynomial() {
         this.terms = new TreeMap<>(Collections.reverseOrder()); // Polynomial is in reverse order using the Hint
         this.degree = -1;
@@ -55,32 +55,7 @@ public class Polynomial implements Comparable<Polynomial> {
         this.terms.put(power, coefficient);
     }
 
-    /** Third Overloaded Constructor
-    * Copy Constructor (The new polynomial is a separate copy of an existing polynomial.)
-    * It has a polynomial, p, as one parameter.
-    * It must create the new polynomial by copying the polynomial given as the parameter.
-    * Use the method putAll to copy all the terms of the existing polynomial to the new one.
-    * @param p an existing polynomial as the source to be copied to the new one
-    */
-    // Third Constructor
-    public Polynomial(Map<Integer, Integer> p) {
-        // Call Default Constructor
-        this();
-
-        // Create the new polynomial by copying the polynomial given as the parameter using putAll() method
-        this.terms.putAll(p);
-        
-        // Since the 'power' variable is in the key element, loop the new 'power' variable in the Map's keySet using the Enchanced loop
-        for (int power : p.keySet()) 
-        {
-            if (power < 0) 
-            {
-                throw new IllegalArgumentException("Power number in Polynomial cannot be negative.");
-            }
-        }
-    }
-    
-    /** Fourth Overloaded Constructor (Copy Constructor)
+    /** Third Overloaded Constructor 
     * Constructs a new polynomial with a TreeMap as a parameter.
     * It must first call the default constructor to create an empty polynomial.
     * Then, it must find the maximum power from the TreeMap given and set the degree of the
@@ -90,36 +65,100 @@ public class Polynomial implements Comparable<Polynomial> {
     * IllegalArgumentException exception with a proper message.
     * @param p a TreeMap including the powers and corresponding coefficients (Integer, Integer)
     */
+    // Third Constructor
+    public Polynomial(Map<Integer, Integer> p) {
+        // Call Default Constructor
+        this();
+
+        // Since the 'power' variable is in the key element, loop the new 'power' variable in the Map's keySet using the Enchanced loop
+        for (int power : p.keySet()) 
+        {
+            // Check if power is less than 0, throw IllegalArgumentException exception
+            if (power < 0) 
+            {
+                throw new IllegalArgumentException("Power number in Polynomial cannot be negative.");
+            }
+
+            // Find max power from TreeMap to set the 'degree' of Polynomial
+            if (power > this.degree) 
+            {
+                this.degree = power;
+            }
+
+            // Add a new term based on the 'power' key variable of the TreeMap
+            terms.put(power, p.get(power));
+        }
+    }
+
+    /** Fourth Overloaded Constructor (Copy Constructor)
+    * Copy Constructor (The new polynomial is a separate copy of an existing polynomial.)
+    * It has a polynomial, p, as one parameter.
+    * It must create the new polynomial by copying the polynomial given as the parameter.
+    * Use the method putAll to copy all the terms of the existing polynomial to the new one.
+    * @param p an existing polynomial as the source to be copied to the new one
+    */
     // Copy Constructor
     public Polynomial(Polynomial p) {
+        // Call Default Constructor
         this();
-        for (int power : p.keySet()) {
-            if (power < 0) {
-                throw new IllegalArgumentException("Power cannot be negative.");
-            }
-            terms.put(power, p.get(power));
-            degree = Math.max(degree, power);
-        }
+
+        // Use the method putAll to copy all the terms of the existing polynomial to the new one.
+        this.terms.putAll(p.terms);
     }
 
+    /** Method Name: add
+    * This method gets a polynomial, p, as a parameter, and adds its terms to the existing
+    * polynomial, and updates the polynomial degree, if needed.
+    * Adds the polynomial such that the terms are in order from highest power to lowest one.
+    * Remember to have no two terms with the same power in the polynomial.
+    * @param p the polynomial to add
+    */
     // Method to add another Polynomial
-    public void add(Polynomial polynomial) {
-        for (int power : polynomial.terms.keySet()) {
-            int coefficient = polynomial.terms.get(power);
-            if (terms.containsKey(power)) {
-                terms.put(power, terms.get(power) + coefficient);
-            } else {
-                terms.put(power, coefficient);
+    public void add(Polynomial p) 
+    {
+        // Since the 'power' variable is in the key element, loop the new 'power' variable in the Map's keySet using the Enchanced loop
+        for (int power : p.terms.keySet()) 
+        {
+            // Get 'coefficient' of the 'p' Polynomial by getting the value maps to a specific key
+            int coefficient = p.terms.get(power);
+            
+            // If the 2 terms has the same key (power) then combine the 2 value (coefficients)
+            if (this.terms.containsKey(power)) 
+            {
+                this.terms.put(power, this.terms.get(power) + coefficient);
+            } 
+            else // else just add them normally 
+            {
+                this.terms.put(power, coefficient);
             }
-            degree = Math.max(degree, power);
+
+            // Find max power from TreeMap to set the 'degree' of Polynomial
+            if (power > this.degree) 
+            {
+                this.degree = power;
+            }
         }
     }
 
+    /** Method Name: add (Overloaded)
+    * This static method gets two polynomials, p1 and p2, as the parameters.
+    * Then, it creates a new polynomial by adding two existing polynomials, stores into the new
+    * one, and returns it.
+    * You must use the add method you have previously developed to prevent any redundant code in
+    * this method.
+    * @param p1 the first existing polynomial
+    * @param p2 the second existing polynomial
+    * @return a new polynomial which is the summation of the two polynomials p1 and p2
+    */
     // Overloaded add method
     public static Polynomial add(Polynomial p1, Polynomial p2) {
-        Polynomial result = new Polynomial(p1);
-        result.add(p2);
-        return result;
+        // Create a new Polynomial which copy from Polynomial p1
+        Polynomial p3 = new Polynomial(p1);
+
+        // Use the add() method to add p2 into p1
+        p3.add(p2);
+        
+        return p3;
     }
 
     // Method to subtract another Polynomial
